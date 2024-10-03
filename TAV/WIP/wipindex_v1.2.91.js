78cbe4cbed302183dfac2b549b9793359fdc077d
mark-mengu <118672530+mark-mengu@users.noreply.github.com>
2024-10-04 01:16:55 +0200

    Update and rename wipindex_v1.2.9.js to wipindex_v1.2.91.js

--- TAV/WIP/wipindex_v1.2.91.js
+++ TAV/WIP/wipindex_v1.2.91.js
@@ -200,7 +200,7 @@ require(['N/https', 'N/url', 'N/search'], (https, url, search) => {
     // };
     // table.addColumn(docValueColumns);
     let trxColumns = {
-        title: "Tipo Transazione", field: "recordtype", editor: "textarea", validator: '',headerFilterPlaceholder: "Filtra una tipo...", editable: false, width: 200, minWidth: 150, maxWidth: 300, headerFilter: "input", formatter: stdBoldFormatter, tooltip: 'Articolo'
+        title: "Tipo Transazione", field: "recordtype", editor: "textarea", validator: '',headerFilterPlaceholder: "Filtra una tipologia...", editable: false, width: 200, minWidth: 150, maxWidth: 300, headerFilter: "input", formatter: stdBoldFormatter, tooltip: 'Articolo'
     };
     table.addColumn(trxColumns);
     let itemColumns = {
