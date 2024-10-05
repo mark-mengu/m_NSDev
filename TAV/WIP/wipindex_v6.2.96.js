7c87dbcf93384563cf4472bbb5a3da65f15c5d87
mark-mengu <118672530+mark-mengu@users.noreply.github.com>
2024-10-06 00:12:38 +0200

    Update and rename wipindex_v6.2.95.js to wipindex_v6.2.96.js

--- TAV/WIP/wipindex_v6.2.96.js
+++ TAV/WIP/wipindex_v6.2.96.js
@@ -186,7 +186,7 @@ var createLoadingIcon = () => {
 //----------------------------------------------------------------TABULATOR-----------------------------------------------------
 const table = new Tabulator("#report-wip", {
     movableRows: false,
-    dataTree: true,
+    dataTree: false,
     dataTreeCollapseElement: `<span class="data-tree-collapse-element"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25"><rect x="2" y="2" width="20" height="20" rx="4" ry="4" fill="#00ff00" /><rect x="6" y="11" width="12" height="2" fill="#FFFFFF" /></svg></span>`,
     dataTreeExpandElement: `<span class="data-tree-collapse-element"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25"><rect x="2" y="2" width="20" height="20" rx="4" ry="4" fill="#ff0000" /><rect x="6" y="11" width="12" height="2" fill="#FFFFFF" /></svg></span>`,
     tabulatorId: "report-wip-table",
