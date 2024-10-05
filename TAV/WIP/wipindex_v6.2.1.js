397043a5bdb79d97b656ad554307a1b2c5872f00
mark-mengu <118672530+mark-mengu@users.noreply.github.com>
2024-10-05 03:40:17 +0200

    Update and rename wipindex_v6.2.0.js to wipindex_v6.2.1.js

--- TAV/WIP/wipindex_v6.2.1.js
+++ TAV/WIP/wipindex_v6.2.1.js
@@ -144,19 +144,18 @@ const table = new Tabulator("#report-wip", {
     dataTree: true,
     dataTreeCollapseElement: `<span class="data-tree-collapse-element"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25"><rect x="2" y="2" width="20" height="20" rx="4" ry="4" fill="#FFC107" /><rect x="6" y="11" width="12" height="2" fill="#FFFFFF" /></svg></span>`,
     dataTreeExpandElement: `<span class="data-tree-collapse-element"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25"><rect x="2" y="2" width="20" height="20" rx="4" ry="4" fill="#FFC107" /><rect x="6" y="11" width="12" height="2" fill="#FFFFFF" /></svg></span>`,
-    dataTreeStartExpanded: function (row, level) { },
     dataTreeFilter: true,
     tabulatorId: "report-wip-table",
     ajaxURL: '',
     ajaxParams: {},
     ajaxFiltering: true,
-    rowHeader: {
-        resizable: true,
-        frozen: true,
-        width: 70,
-        formatter: (cell) => { let rowNumber = cell.getRow().getPosition(); },
-        hozAlign: "center"
-    },
+    // rowHeader: {
+    //     resizable: true,
+    //     frozen: true,
+    //     width: 70,
+    //     formatter: (cell) => { let rowNumber = cell.getRow().getPosition(); },
+    //     hozAlign: "center"
+    // },
     selectableRangeRows: false,
     columnDefaults: { headerSort: true, resizable: "header" },
     dataLoaderLoading: "Loading data...",
