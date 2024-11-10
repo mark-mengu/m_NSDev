9d05b744abda7ca1f66ecf6f4fc73d9e64bca156
mark-mengu <118672530+mark-mengu@users.noreply.github.com>
2024-11-10 18:09:47 +0100

    Add files via upload

---
+++ TADI/UTILISCustomer/indexcustv1.0.js
@@ -0,0 +1,290 @@
+//--------------------------------------------------------------RAWss FUNCTIONS-----------------------------------------
+var validate = (cell) => {
+    let q = cell.getValue();
+    console.log('cell.getData()', cell.getData());
+    return true;
+}
+
+var editCheck = (cell) => {
+    return !cell.getRow().getData().hold;
+}
+
+var printInventoryNumber = (cell, formatterParams, onRendered) => {
+    return cell.getColumn().getDefinition().editorParams.values[cell.getValue()];
+};
+
+var printBin = (cell, formatterParams, onRendered) => {
+    return cell.getColumn().getDefinition().editorParams.values[cell.getValue()];
+};
+
+var printIcon = (cell, formatterParams) => {
+    return "<i class='fa fa-print'></i>";
+};
+
+var linkFormatter = (cell, formatterParams) => {
+    let value = cell.getValue();
+    let url = 'https://example.com/' + encodeURIComponent(value);
+    cell.getElement().style.backgroundColor = "#CACAEE";
+    return `<a href="${url}" target="_blank">${value}</a>`;
+};
+
+var stdFormatter = (cell, formatterParams) => {
+    let value = cell.getValue();
+    cell.getElement().style.backgroundColor = "#CACAEE";
+    return value;
+};
+
+var salesorderFormatter = (cell, formatterParams) => {
+    let value = cell.getValue();
+    cell.getElement().style.backgroundColor = "#CACAEE";
+    let button = '<button id="opensss" class="sexy-button" onclick="openWind(event, \'' + value + '\')">Apri Ordine di Vendita</button>';
+    return button;
+};
+
+var customerFormatter = (cell, formatterParams) => {
+    let value = cell.getValue();
+    cell.getElement().style.backgroundColor = "#CACAEE";
+    return '<u>' + value + '</u>';
+};
+
+var inventoryValueFormatter = (cell, formatterParams) => {
+    let value = cell.getValue();
+    cell.getElement().style.backgroundColor = "#CACAEE";
+    return parseFloat(value).toFixed(2);
+};
+
+var openWind = (event, url) => {
+    event.preventDefault();
+    window.open(url, '_blank');
+}
+var dataFilter = (headerValue, rowValue, rowData, filterParams) => {
+    //headerValue - the value of the header filter element
+    //rowValue - the value of the column in this row
+    //rowData - the data for the row being filtered
+    //filterParams - params object passed to the headerFilterFuncParams property
+
+    return rowData.name == filterParams.name && rowValue < headerValue;
+}
+var createLoadingIcon = () => {
+    const loadingIcon = document.createElement('div');
+    loadingIcon.id = 'loading-icon';
+    loadingIcon.style.cssText = `
+    display: none;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+  `;
+    const spinner = document.createElement('div');
+    spinner.style.cssText = `
+    border: 7px solid #f3f3f3;
+    border-top: 7px solid #eb9534;
+    border-radius: 55%;
+    width: 75px;
+    height: 75px;
+    animation: spin 1s linear infinite;
+  `;
+    loadingIcon.appendChild(spinner);
+    document.body.appendChild(loadingIcon);
+    const style = document.createElement('style');
+    style.textContent = `
+    @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+  `;
+    document.head.appendChild(style);
+    return loadingIcon;
+}
+//----------------------------------------------------------------TABULATOR-----------------------------------------------------
+
+const table = new Tabulator("#report-deposito", {
+    movableRows: false,
+    groupToggleElement: true,
+    tabulatorId: "report-deposito-table",
+    rowHeader: {
+        resizable: true,
+        frozen: true,
+        width: 70,
+        formatter: (cell) => {
+            let rowNumber = cell.getRow().getPosition();
+            return '<div class="row-index">' + rowNumber + '</div>';
+        },
+        hozAlign: "center"
+    },
+    selectableRangeRows: false,
+    columnDefaults: { headerSort: true, resizable: "header" },
+    dataLoaderLoading: "Loading data...",
+    placeholder: "No DATA Found...",
+    pagination: "local",
+    paginationSize: 50,
+    //frozenRows:1,
+    //groupBy: "customer",
+    //groupByHeader: true,
+    ajaxProgressiveLoad: "scroll",
+    printFooter: "",
+    printHeader: "<center><h1>Conto Deposito Overview</h1></center>",
+    rowFormatter: (row) => {
+        let data = row.getData();
+        if (data.inv_text == ' ') {
+            let cells = row.getCells();
+            cells.forEach(cell => {
+                cell.getElement().style.color = "red";
+                cell.getElement().style.fontWeight = "bold";
+            });
+        }
+    }
+});
+
+document.getElementById('report-deposito').style.display = 'none';
+document.getElementById('table-title').style.display = 'none';
+require(['N/https', 'N/url', 'N/currentRecord'], (https, url, cr) => {
+    let resourcesUrl = url.resolveScript({
+        scriptId: 'customscript_gn_ta_conto_deposito_data',
+        deploymentId: 'customdeploy_gn_ta_conto_deposito_data',
+        params: {
+            date: cr.get().getValue('custpage_date'),
+            customer: cr.get().getValue('custpage_customer'),
+            item: cr.get().getValue('custpage_item'),
+            serial: cr.get().getValue('custpage_serial'),
+        }
+    });
+    let docValueColumns = {
+        title: " ", field: "so_link", editor: "textarea", validator: '', editable: false, headerFilter: "", formatter: salesorderFormatter, tooltip: 'Vedi Documento'
+    };
+    table.addColumn(docValueColumns);
+    let soValueColumns = {
+        title: "Sales Order", field: "so_text", editor: "textarea", validator: '', width: 280, minWidth: 200, maxWidth: 400, editable: false, headerFilter: "input", formatter: stdFormatter, tooltip: 'Sales Order Collegato'
+    };
+    table.addColumn(soValueColumns);
+    let invValueColumns = {
+        title: "Invoice", field: "inv_text", editor: "textarea", validator: '', editable: false, headerFilter: "input", formatter: stdFormatter, tooltip: 'Invoice Collegata'
+    };
+    table.addColumn(invValueColumns);
+    let customerColumn = {
+        title: "Cliente", field: "customer", editor: "textarea", validator: '', width: 280, minWidth: 200, maxWidth: 400, editable: false, headerFilter: "input",
+        formatter: customerFormatter,
+        tooltip: 'Cliente',
+    };
+    table.addColumn(customerColumn);
+    let machineColumns = {
+        title: "Articolo", field: "item", editor: "textarea", validator: '', editable: false, headerFilter: "input", formatter: stdFormatter, tooltip: 'Articolo'
+    };
+    table.addColumn(machineColumns);
+    let displaynameColumns = {
+        title: "Descrizione Articolo", field: "displayname", editor: "textarea", validator: '', width: 300, minWidth: 200, maxWidth: 400, editable: false, headerFilter: "input",
+        formatter: stdFormatter,
+        tooltip: 'Descrizione Articolo'
+    };
+    table.addColumn(displaynameColumns);
+    let serialColumns = {
+        title: "Seriale", field: "seriale", editor: "textarea", validator: '', editable: false, headerFilter: "input", formatter: stdFormatter, tooltip: 'Seriale'
+    };
+    table.addColumn(serialColumns);
+    let quantityColumn = {
+        title: "Quantità", field: "quantity", editor: "number", validator: '', editable: false, formatter: stdFormatter, tooltip: 'Quantità', bottomCalc: ''
+    };
+    table.addColumn(quantityColumn);
+    let unitsColumn = {
+        title: "U.tà", field: "units", editor: "textarea", validator: '', editable: false, formatter: stdFormatter, tooltip: 'U.tà'
+    };
+    table.addColumn(unitsColumn);
+
+    const loadingIcon = createLoadingIcon();
+    const reportDeposito = document.getElementById('report-deposito');
+    const tableTitle = document.getElementById('table-title');
+    loadingIcon.style.display = 'block';
+    reportDeposito.style.display = 'none';
+    tableTitle.style.display = 'none';
+    https.get.promise({ url: resourcesUrl })
+        .then((response) => {
+            let data = JSON.parse(response.body);
+            table.setData(data.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+        .finally(() => {
+            loadingIcon.style.display = 'none';
+            reportDeposito.style.display = 'block';
+            tableTitle.style.display = 'block';
+        });
+});
+//------------------------------------------------------------------EDIT------------------------------------------------------
+table.on("cellEdited", (cell) => {
+    //cell.getData()['edit'] = true;
+});
+
+//-----------------------------------------------------------------PRINT PDF-------------------------------------------------------------------------------
+
+document.getElementById('print-pdf').addEventListener('click', (event) => {
+    table.hideColumn("to");
+    table.hideColumn("so_text");
+    table.hideColumn("item_value");
+    table.hideColumn("units");
+
+    table.download("pdf", "report_deposito.pdf", { title: "Report Controllo Merce in Conto Deposito" });
+
+    table.toggleColumn("to");
+    table.toggleColumn("so_text");
+    table.toggleColumn("item_value");
+    table.toggleColumn("units");
+    event.preventDefault();
+}, false);
+
+//-----------------------------------------------------------------PRINT XLS-------------------------------------------------------------------------------
+
+document.getElementById('print-xls').addEventListener('click', (event) => {
+    const columnsToHide = ["to"];
+    columnsToHide.forEach(column => table.hideColumn(column));
+
+    table.download("xlsx", "report_deposito.xlsx", { sheetName: "Report Deposito", bom: true });
+
+    columnsToHide.forEach(column => table.showColumn(column));
+    event.preventDefault();
+
+}, false);
+
+//--------------------------------------------------------------EDITING NOT USED----------------------------------------------------------------------
+/*
+document.getElementById("").addEventListener("click", (event) => {
+  event.preventDefault();
+  require(['N/https', 'N/url', 'N/currentRecord', 'N/ui/dialog'], (https, url, cr, dialog) => {
+    const scriptFix = document.createElement('script');
+    scriptFix.src = "https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js";
+    scriptFix.onload = () => {
+      const _ = window._;
+      let binDatas = _.some(table.getData(), obj => _.has(obj, 'bin') && (obj.bin == null || obj.bin === '' || obj.bin === undefined));
+      if (binDatas) {
+        dialog.alert({
+          title: 'ATTENZIONE',
+          message: `<center><img src="https://9094479.app.netsuite.com/core/media/media.nl?id=3105&c=9094479&h=lZQSbeBwUYC4IQL-ZVNFLj9yHvee0pyclIO65T5hPbXxV0EY&fcts=20240401080554&whence=" width="150" height="150"> <div style="color:yellow; background-color:gray; padding:10px; border-radius:10px;"><b>Non è possibile procedere con il controllo, non è stato inserito il BIN in tutte le righe.</b></div></center>`,
+        });
+        return false;
+      }
+      let resourcesUrl = url.resolveScript({
+        scriptId: 'customscript_gn_tav_quality_control_data',
+        deploymentId: 'customdeploy_gn_tav_quality_control_data',
+      });
+      let body = {
+        data: table.getData(),
+        qualitycontrol: cr.get().getValue('custpage_quality_control'),
+        item: cr.get().getValue('custpage_item'),
+        transaction: cr.get().getValue('custpage_transaction'),
+        line: cr.get().getValue('custpage_line')
+      };
+      let response = https.post({ url: resourcesUrl, body: JSON.stringify(body) });
+      table.setData(JSON.parse(response.body).data);
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Quality Control has been saved",
+        showConfirmButton: false,
+        timer: 2400
+      });
+      table.clearAlert();
+    };
+    document.head.appendChild(scriptFix);
+  });
+}, false);*/
