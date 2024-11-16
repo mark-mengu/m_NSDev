1841ff76a4181aa35c0d46ad2e9b9520f93099f6
mark-mengu <118672530+mark-mengu@users.noreply.github.com>
2024-11-16 22:54:47 +0100

    Update and rename indexv1.9.92.js to indexv1.9.93.js

--- TADI/UTILS/indexv1.9.93.js
+++ TADI/UTILS/indexv1.9.93.js
@@ -48,14 +48,26 @@ var customerFormatter = (cell, formatterParams) => {
 };
 
 var salesOrderFormatter = (cell, formatterParams) => {
+    // Recupera il valore del campo dalla cella
     let so_consegna_link = cell.getValue();
-    let parts = so_consegna_link.split('|').map(value => value || "");
-    //SO TEXT|SO|TO|ITEM
-    let link = `<a href="https://6518658.app.netsuite.com/app/common/search/searchresults.nl?searchtype=Transaction&IT_Item_NAME=&CU_Entity_ENTITYID=&AFC_Transaction_NUMBERTEXT=${parts[2]}&Transaction_NUMBERTEXT=&style=NORMAL&IT_Item_NAMEtype=CONTAINS&CU_Entity_ENTITYIDtype=CONTAINS&AFC_Transaction_NUMBERTEXTtype=CONTAINS&Transaction_NUMBERTEXTtype=CONTAINS&report=&grid=&searchid=2435&dle=T&sortcol=Transction_DATATED11_raw&sortdir=DESC&csv=HTML&OfficeXML=F&pdf=&size=1000&_csrf=nRz36NGWjWsvP7GzsEiTlgPOWs0ch0TQ6oRNqk9S4nnJHBgZd3NMuSQsJFgKf33phq1f7N9dpkE2KC20mt1IqfIIoLZD5V_MX8Euk8k384S6lIbklLz8sORggkYkhTGTo6zgkkzfQ2jFOlpvDY0eyIdnXHNHcjMhPCP7EMUIJLs%3D&twbx=F" target="_blank">${parts[0]}</a>`;
 
+    // Separa i dati ottenuti dal valore
+    let parts = so_consegna_link.split('|').map(value => value || "");
+
+    // Crea il link HTML con il formato desiderato
+    let link = `<a href="https://6518658.app.netsuite.com/app/common/search/searchresults.nl?searchtype=Transaction&IT_Item_NAME=&CU_Entity_ENTITYID=&AFC_Transaction_NUMBERTEXT=${parts[2]}&Transaction_NUMBERTEXT=&style=NORMAL&IT_Item_NAMEtype=CONTAINS&CU_Entity_ENTITYIDtype=CONTAINS&AFC_Transaction_NUMBERTEXTtype=CONTAINS&Transaction_NUMBERTEXTtype=CONTAINS&report=&grid=&searchid=2435&dle=T&sortcol=Transction_DATATED11_raw&sortdir=DESC&csv=HTML&OfficeXML=F&pdf=&size=1000&_csrf=nRz36NGWjWsvP7GzsEiTlgPOWs0ch0TQ6oRNqk9S4nnJHBgZd3NMuSQsJFgKf33phq1f7N9dpkE2KC20mt1IqfIIoLZD5V_MX8Euk8k384S6lIbklLz8sORggkYkhTGTo6zgkkzfQ2jFOlpvDY0eyIdnXHNHcjMhPCP7EMUIJLs%3D&twbx=F" target="_blank" style="color: #4CAF50; font-weight: bold; text-decoration: none; padding: 4px 8px; border-radius: 4px; background-color: #f0f0f0; display: inline-block; box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1); transition: background-color 0.3s, box-shadow 0.3s;">
+        ${parts[0]} 
+    </a>`;
+
+    // Modifica lo stile della cella
     cell.getElement().style.backgroundColor = "#CACAEE";
-    return '<u>' + link + '</u>';
+    cell.getElement().style.padding = "8px";
+    cell.getElement().style.borderRadius = "4px";
+
+    // Restituisci il link con il formato migliorato
+    return link;
 };
+
 
 var invoiceDateFormatter = (cell, formatterParams) => {
     let value = cell.getValue();
